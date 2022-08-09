7fa7c277632dd4a663e97a02db34ecf0f73df640
mirmahmud2007 <mirmahmudmirsali@gmail.com>
2022-08-09 22:31:05 +0500

    3-dars.js

---
+++ main.js
@@ -0,0 +1,18 @@
+var HowMuchMoneyDoYouHave = prompt("QANCHA PULING BOR?");
+var ruble = 5000;
+var dollar = 91.869;
+var euro = 89.475;
+
+var ruble = 5000 * 184.22;
+var dollar = 91.869 * 10855.23;
+var euro = 89.475 * 11176.22;
+
+var count = Number(ruble + dollar + euro);
+
+if (HowMuchMoneyDoYouHave >= count) {
+  alert("PULING YETADI, LIKIN JS DARSLARIDAN BARIBIR QOCHIB KETOLMAYSAN! 🥳");
+} else if (HowMuchMoneyDoYouHave <= 0) {
+  alert("PUL ETMAYDI URUNMA! 😞");
+}else {
+  alert("PUL YETMAS EKAN, ENDI JS DARSLARIDAN QOCHIB KETOLMAYSAN! 😔");
+}
